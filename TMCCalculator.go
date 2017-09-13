4c7e27f393470f8e010b7d1e8a226dd17e9bd2d9
Byron <byroncy@gmail.com>
2017-09-13 10:37:33 +0800

    before using Array

--- TMCCalculator.go
+++ TMCCalculator.go
@@ -15,14 +15,14 @@ func main() {
 
 	fmt.Print("Start TMC Calculation!")
 	//define variable
-	orderNum := "MU5354123456"
-	flightNum := "MU5354"
-	departureCity := "美国"
-	arrivalCity := "武汉"
-	flightPrice := "1070"
-	username := "解华朋"
+	orderNum := "FM9451"
+	flightNum := "FM9451"
+	departureCity := "上海"
+	arrivalCity := "西安"
+	flightPrice := "1610"
+	username := "赵楠"
 	hongbaoNum := "5.88"
-	phoneNum := "15558036555"
+	phoneNum := "18961470955"
 
 	part01 := "INSERT INTO `hd_hongbao`.`orders` (`id`, `user_name`, `id_card_number`, `phone_number`, `provider_id`, `service_id`, `order_number`, `order_detail`, `hongbao_given`, `hongbao_used`, `created`, `modified`) VALUES (NULL, '"
 	part02 := "', '111', '"
@@ -32,7 +32,7 @@ func main() {
 	part06 := "\\\", \\\"flight_city_arrival\\\" : \\\""
 	part07 := "\\\",\\\"flight_class\\\" : \\\"经济舱\\\", \\\"flight_price\\\" : \\\""
 	part08 := "\\\", \\\"flight_tax\\\" : \\\"0\\\"}', '"
-	part09 := "', '0', '2017-09-05 14:55:50', '2017-09-05 14:55:50');"
+	part09 := "', '0', '2017-08-19 09:00:00', '2017-08-19 09:00:00');"
 
 	finalStr := part01 + username + part02 + phoneNum + part03 + orderNum + part04 + flightNum + part05 + departureCity + part06 + arrivalCity + part07 + flightPrice + part08 + hongbaoNum + part09
 
